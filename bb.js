bcd56eff637acc22d86281e69b95956939cc74f4
b-breil <bernhardbreil@googlemail.com>
2024-11-25 12:39:15 +0100

    a js

---
+++ bb.js
@@ -0,0 +1,30 @@
+window.onload = function zeigeWillkommen() {
+    console.log("Willkommen auf der Seite der Apotheke zur Gesundheit!");
+};
+
+function zeigeDetails(){
+    document.getElementById("details").text = "adsf asdf adsf";
+}
+
+
+function check(){
+
+    if(document.getElementById("email").value == "")
+        alert("Bitte geben Sie einen Text ein");
+
+}
+
+function schleife(){
+
+    //For-Schleife in JS
+
+    text = "";
+
+    for(var i=0;i <10; i++){
+
+       text = text + "Ich bin Zeile "+i;
+  
+    }
+    document.getElementById("email").value = text;
+
+}
